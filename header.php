691b1aa7b0f2d16b5c22e4e3e6f51257bf19a44a
benson.rc86 <benson.rc86@884b0509-731a-a738-1c0a-efb61104bd45>
2010-12-06 15:50:47 +0000

    主界面样式修改；

--- header.php
+++ header.php
@@ -1,5 +1,12 @@
 <div class="header">
 <style type="text/css">
 .header { position: absolute; top: 0; height: 30px; width: 100%; background-color: #CCC; }
+ul { margin: 0; padding: 0; }
+li { list-style: none; }
+.herizontal-list li { float: right; }
 </style>
+<ul class="herizontal-list">
+	<li>abc</li>
+	<li>def</li>
+</ul>
 </div>
