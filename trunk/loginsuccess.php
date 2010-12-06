cda2954ce216d9d49eb3e1484ab7bb2cb6466dc9
benson.rc86 <benson.rc86@884b0509-731a-a738-1c0a-efb61104bd45>
2010-12-06 15:50:47 +0000

    主界面样式修改；

--- trunk/loginsuccess.php
+++ trunk/loginsuccess.php
@@ -4,6 +4,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>登录成功</title>
 <link type="text/css" rel="stylesheet" href="styles/global.css" />
+s<script type="text/javascript" src="scripts/jquery.js"></script>
+<script type="text/javascript" src="scripts/global.js"></script>
 </head>
 <body>
 <?php include 'header.php'; ?>
--- trunk/scripts/global.js
+++ trunk/scripts/global.js
@@ -1,4 +1,9 @@
 // JavaScript Document 中文
+window.onresize = function() {
+	$('.leftbar, .main').height($(window).height() - 30);
+}
+
 $(function(){
+	$('.leftbar, .main').height($(window).height() - 30);
 	$('input[type=text]').eq(0).focus();
 });
