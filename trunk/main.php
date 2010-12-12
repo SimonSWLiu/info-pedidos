60d7a109f418e8755fef05d5514c70ef0d2e2e7c
benson.rc86 <benson.rc86@884b0509-731a-a738-1c0a-efb61104bd45>
2010-12-12 16:28:06 +0000

    增加“增加餐厅名”功能；

--- trunk/main.php
+++ trunk/main.php
@@ -4,5 +4,5 @@
 .main { position: relative; margin-left: 200px; margin-top: 30px; height: 570px; width: auto; background-color: #00F; }
 .main-frame { position: relative; width: 100%; height: 330px; }
 </style>
-	<iframe src="http://fit4.cn" name="main_frame" class="main-frame" frameborder="0" scrolling="auto"></iframe>
+	<iframe src="menumanage.php" name="main_frame" class="main-frame" frameborder="0" scrolling="auto"></iframe>
 </div>
