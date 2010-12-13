ceba4e751cc42bd3612c2d2ad20a1efc7371e4ee
benson.rc86 <benson.rc86@884b0509-731a-a738-1c0a-efb61104bd45>
2010-12-13 15:13:02 +0000

    完成删除餐厅的功能；

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <!--主窗口-->
 <div class="main">
 <style type="text/css">
-.main { position: relative; margin-left: 200px; margin-top: 30px; height: 570px; width: auto; background-color: #00F; }
+.main { position: relative; margin-left: 200px; margin-top: 30px; height: 570px; width: auto; }
 .main-frame { position: relative; width: 100%; height: 330px; }
 </style>
 	<iframe src="menumanage.php" name="main_frame" class="main-frame" frameborder="0" scrolling="auto"></iframe>
--- scripts/global.js
+++ scripts/global.js
@@ -7,4 +7,15 @@ window.onresize = function() {
 $(function(){
 	window.onresize();
 	$('input[type=text]').eq(0).focus();
+	
+	// 点击删除餐厅名功能
+	$('.del-restaurant').click(function(){
+		$del = $(this);
+		$.get('/delr.php', {
+			restaurant: $del.attr('rid')
+		}, function(data){
+			alert('success');
+			window.location.reload();
+		}, 'json');
+	});
 });
