151ca15468b92fee3654d236a4dc3d1a03bffba7
benson.rc86 <benson.rc86@884b0509-731a-a738-1c0a-efb61104bd45>
2011-01-05 16:31:32 +0000

    更改样式

--- trunk/login.php
+++ trunk/login.php
@@ -2,11 +2,6 @@
 include 'config.php';
 include 'db.php';
 if (isset($_SESSION['login'])) header('location: /pedidos.php');
-//if (isset($_SESSION['login'])) {
-//	echo '<br />';
-//	exit('success');
-//}
-//else exit('fail');
 if ($_POST) {
 	$user = isset($_POST['user'])? $_POST['user']:'';
 	$pwd = isset($_POST['pwd'])? $_POST['pwd']:'';
@@ -18,13 +13,13 @@ if ($_POST) {
 	$sql = "SELECT * FROM members WHERE (email='$user' AND pwd='$md5_pwd') OR (name='$user' AND pwd='$md5_pwd')";
 	$result = $db->query($sql);
 	mysqli_close($db);
-	if ($result) { // 登录成功
+	$row = $result->fetch_assoc();
+	if ($row) { // success
 		// 写入session
-		$row = $result->fetch_assoc();
 		$_SESSION['login'] = array('mid'=>$row['mid'], 'email'=>$row['email'], 'name'=>$row['name'], 'status'=>$row['status'], 'level'=>$row['level']);
 		header('location: /pedidos.php');
 		exit('login success');
-	} else { // 登录失败
+	} else {
 		exit('login fail');
 	}
 }
@@ -41,8 +36,8 @@ if ($_POST) {
 <div class="login-win">
 	<span><?= isset($_GET['msg'])? $_GET['msg']:''; ?></span>
 	<form action="" method="post">
-		<label for="user">用户名: </label><input type="text" name="user" id="user" /><br />
-		<label for="user">密码: </label><input type="password" name="pwd" id="pwd" /><br />
+		<label for="user">用户名: </label><br /><input type="text" name="user" id="user" /><br />
+		<label for="user">密码: </label><br /><input type="password" name="pwd" id="pwd" /><br />
 		<input type="checkbox" /><label for="remember">下次记住我</label><br />
 		<input type="submit" value="登 录" />
 	</form>
