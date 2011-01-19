d3416c2bfe4a922fc638a7c6355617d6f8f08528
benson.rc86 <benson.rc86@884b0509-731a-a738-1c0a-efb61104bd45>
2011-01-19 13:36:42 +0000

    数据库修改； 新增功能；

--- trunk/delr.php
+++ trunk/delr.php
@@ -1,8 +1,11 @@
 <?php
 include 'config.php';
+include 'db.php';
 if ($_GET) {
 	$rid = $_GET['restaurant'];
 	$sql = "DELETE FROM restaurant WHERE rid='$rid'"; // 删除一条餐厅名记录
-	$db->query($sql);
+	$row_affected = $db->query($sql);
+	if ($row_affected == 1) header('location:' . $_SERVER['HTTP_REFERER']);
+	else exit('出错');
 }
 ?>
