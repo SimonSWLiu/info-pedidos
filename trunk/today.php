d3bcb1fddba0aa816d3f6f4faa411ca2c30df337
benson.rc86 <benson.rc86@884b0509-731a-a738-1c0a-efb61104bd45>
2011-04-20 10:41:43 +0000

    备份数据库； 新增“今日点餐”页面；

---
+++ trunk/today.php
@@ -0,0 +1,47 @@
+<?php
+// 今日点餐
+include 'config.php';
+include 'db.php';
+$myId = $_SESSION['login']['mid'];
+$dateArr = getdate();
+$year = $dateArr['year'];
+$mon = $dateArr['mon'];
+$day = $dateArr['mday'];
+$sql = "SELECT * FROM pedidos_log WHERE mid='$myId' AND year='$year' AND month='$mon' AND day='$day'";
+$result = mysqli_query($db, $sql);
+$menuArr = array();
+while ($row = mysqli_fetch_assoc($result)) $menuArr[] = $row;
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>今日点餐</title>
+<link type="text/css" rel="stylesheet" href="styles/global.css" />
+</head>
+<body>
+	<table border="1">
+		<tr>
+			<th>餐厅</th>
+			<th>类别</th>
+			<th>菜式</th>
+			<th>下单时间</th>
+			<th>单价</th>
+			<th>数量</th>
+			<th>总价</th>
+			<th>状态</th>
+		</tr>
+		<tr>
+		<?php foreach ($menuArr as $row): ?>
+			<td><?php echo $row['r_name']; ?></td>
+			<td><?php echo $row['c_name']; ?></td>
+			<td><?php echo $row['dish_name']; ?></td>
+			<td><?php echo date("Y-m-d H:i:s", $row['edit_time']); ?></td>
+			<td><?php echo $row['unit_price']; ?></td>
+			<td><?php echo $row['dish_count']; ?></td>
+			<td><?php echo $row['total_price']; ?></td>
+			<td><?php echo $row['status']; ?></td>
+		<?php endforeach; ?>
+		</tr>
+	</table>
+</body>
+</html>
