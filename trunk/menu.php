c9e9f6fefd97b5a6fe6700dd4894f4ae8e235510
benson.rc86@gmail.com <benson.rc86@gmail.com@884b0509-731a-a738-1c0a-efb61104bd45>
2012-01-06 11:19:02 +0000

    随机餐厅

--- trunk/menu.php
+++ trunk/menu.php
@@ -40,61 +40,17 @@ if ($_POST) {
 	}
 	setcookie ( 'pedidos', $pedidos, time () + 41400 );
 	exit ( '<script>parent.location.href="pedidos.php?from=' . urlencode($_SERVER['HTTP_REFERER']) . '"</script>' );
-	
-//	if ($_POST ['step'] == 4) {
-//		$menuArr = ( array ) $_POST ['selectMenu']; // 获取提交的菜式
-//		
-//
-//		$listStr = isset ( $_COOKIE ['pedidos'] ) ? $_COOKIE ['pedidos'] : ''; // cookie中已点菜单
-//		
-//
-//		$listArr = array ();
-//		$menus = array ();
-//		$listArr = explode ( ';', $listStr );
-//		array_pop ( $listArr );
-//		
-//		foreach ( $listArr as $row ) { // 将已点菜单字符串分解成数组
-//			$rows = explode ( ':', $row );
-//			$menu ['menu_id'] = $rows [0];
-//			$menu ['menu_count'] = $rows [1];
-//			$menus [] = $menu;
-//		}
-//		
-//		$arrCount = $count = count ( $menus );
-//		foreach ( $menuArr as $row ) {
-//			$tag = 0;
-//			for($i = 0; $i < $arrCount; $i ++) {
-//				if ($row == $menus [$i] ['menu_id']) {
-//					$menus [$i] ['menu_count'] += 1;
-//					$i = $arrCount;
-//					$tag = 1;
-//				}
-//			}
-//			if ($tag == 0) {
-//				$menu ['menu_id'] = $row;
-//				$menu ['menu_count'] = 1;
-//				$menus [] = $menu;
-//			}
-//		}
-//		
-//		$pedidos = '';
-//		foreach ( $menus as $row ) {
-//			$pedidos .= $row ['menu_id'] . ':' . $row ['menu_count'] . ';';
-//		}
-//		
-//		setcookie ( 'pedidos', $pedidos, time () + 41400, '/' );
-//		exit ( '<script>parent.location.href="/pedidos.php"<//script>' );
-//	}
-
 }
-
-$param=isset($_GET['restaurant'])? $_GET['restaurant']:'1';
 
 $sql = "SELECT * FROM restaurant";
 $result = $db->query ( $sql );
 while ( ($row = $result->fetch_assoc ()) == true ) {
-	$rest [$row ['rid']] = $row ['r_name'];
+//	$rest [$row ['rid']] = $row ['r_name'];
+	$rest[] = array('rid'=>$row['rid'], 'r_name'=>$row['r_name']);
 }
+shuffle($rest);
+
+$param=isset($_GET['restaurant'])? $_GET['restaurant']:$rest[0]['rid'];
 
 $param=addslashes($param);
 $sql1 = "SELECT * FROM category WHERE rid='$param'";
@@ -127,8 +83,8 @@ $notice_content = $notice_arr['notice_content'];
 	<div class="restaurant-select">
 		<label>餐厅：
 			<select name="restaurant_name" id="restaurant_name">
-				<?php foreach($rest as $key=>$row): ?>
-				<option value="<?php echo $key; ?>" <?php if($key==$param) echo 'selected="selected"'; ?>><?php echo $row; ?></option>
+				<?php foreach($rest as $row): ?>
+				<option value="<?php echo $row['rid']; ?>" <?php if($row['rid']==$param) echo 'selected="selected"'; ?>><?php echo $row['r_name']; ?></option>
 				<?php endforeach; ?>
 			</select>
 		</label>
